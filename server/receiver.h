58acfe436a6ece683dd002c7dc3756ed3ce406b9
Oli03 <oweber@fi.uba.ar>
2026-05-15 16:38:31 -0300

    feat: Inicio Server

---
+++ server/receiver.h
@@ -0,0 +1,23 @@
+#ifndef RECEIVER_H
+#define RECEIVER_H
+
+#include <string>
+
+#include "../common/queue.h"
+#include "../common/thread.h"
+
+#include "protocolo_server.h"
+
+class Receiver: public Thread {
+private:
+    ProtocoloServer& protocolo;
+    Queue<std::string>& comandos;
+    const int idCliente;
+
+public:
+    Receiver(ProtocoloServer& protocolo, Queue<std::string>& comandos, const int idCliente);
+
+    virtual void run() override;
+};
+
+#endif
