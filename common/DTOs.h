71c65c40311ca4e0d8e41037b29e66615dd1731b
nicolasmdelatorre <nicolasmdelatorre@gmail.com>
2026-05-20 10:18:20 -0300

    Modifico Thread FIle

---
+++ common/DTOs.h
@@ -0,0 +1,13 @@
+//
+// Created by nicolas on 19/5/26.
+//
+
+#ifndef TP_GRUPAL_GRUPO_09_ARGENTUM_DTOS_H
+#define TP_GRUPAL_GRUPO_09_ARGENTUM_DTOS_H
+
+
+enum Command{};
+
+enum ServerMessageType{};
+
+#endif //TP_GRUPAL_GRUPO_09_ARGENTUM_DTOS_H
